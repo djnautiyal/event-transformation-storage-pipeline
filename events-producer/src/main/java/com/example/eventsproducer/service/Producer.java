34842339e513664ab78ed71e2417763f8c3f6540
djnautiyal <dhannanjainautiyal@gmail.com>
2023-08-02 10:38:04 +0530

    logger added

--- events-producer/src/main/java/com/example/eventsproducer/service/Producer.java
+++ events-producer/src/main/java/com/example/eventsproducer/service/Producer.java
@@ -25,9 +25,9 @@ public class Producer {
         CompletableFuture<SendResult<String, String>> future = kafkaTemplate.send(topicName, message);
             future.whenComplete((result, ex) -> {
                 if (ex == null) {
-                    System.out.println("Sent message=[" + message + "] with offset=[" + result.getRecordMetadata().offset() + "]");
+                    logger.info("Sent message=[" + message + "] with offset=[" + result.getRecordMetadata().offset() + "]");
                 } else {
-                    System.out.println("Unable to send message=[" + message + "] due to : " + ex.getMessage());
+                    logger.info("Unable to send message=[" + message + "] due to : " + ex.getMessage());
                 }
             });
     }
