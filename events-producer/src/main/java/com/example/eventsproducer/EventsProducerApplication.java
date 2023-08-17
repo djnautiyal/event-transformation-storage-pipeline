5455d1f6a32d8008303f0c69f7c429b997bc7ec3
djnautiyal <dhannanjainautiyal@gmail.com>
2023-08-17 21:57:34 +0530

    /publish works as intended. /read doesn't. Because of Kafka listener

--- events-producer/src/main/java/com/example/eventsproducer/EventsProducerApplication.java
+++ events-producer/src/main/java/com/example/eventsproducer/EventsProducerApplication.java
@@ -9,23 +9,12 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.util.concurrent.TimeUnit;
 
-//docker compose -f docker-compose-multi-node.yaml up -d
 
-@RestController
-@SpringBootApplication()
+@SpringBootApplication
 public class EventsProducerApplication {
 
 
     public static void main(String[] args) throws InterruptedException {
-        ConfigurableApplicationContext context = SpringApplication.run(EventsProducerApplication.class, args);
-        Producer producer = context.getBean(Producer.class);
-        Consumer consumer = context.getBean(Consumer.class);
-
-        for(int i=0; i<10; i++)
-            producer.sendMessage("Hello, Kafka! Times: " + Double.valueOf(Math.random()*10000).intValue());
-
-        consumer.latch.await(15, TimeUnit.SECONDS);
-
-        context.close();
+        SpringApplication.run(EventsProducerApplication.class, args);
     }
 }
