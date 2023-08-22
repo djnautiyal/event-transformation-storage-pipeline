3b5766068c2ec7efe3e9e3a1fe2fac24609641d3
djnautiyal <dhannanjainautiyal@gmail.com>
2023-08-22 22:57:44 +0530

    Updating producer

--- events-producer/src/main/java/com/example/eventsproducer/controller/ApiController.java
+++ events-producer/src/main/java/com/example/eventsproducer/controller/ApiController.java
@@ -1,6 +1,5 @@
 package com.example.eventsproducer.controller;
 
-import com.example.eventsproducer.service.Consumer;
 import com.example.eventsproducer.service.Producer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -20,21 +19,11 @@ public class ApiController {
     @Autowired
     private Producer producer;
 
-    @Autowired
-    private Consumer consumer;
-
     @GetMapping("/publish")
     public String sendMessageToKafkaTopic(/*@RequestParam("message") String message : Later*/) {
         logger.info("running producer");
         for(int i=0; i<10; i++)
             this.producer.sendMessage("Hello, Kafka! Random number: " + Double.valueOf(Math.random()*10000).intValue());
         return "We sent the message. Hopefully, you can read them.";
-    }
-
-    @GetMapping("/read")
-    public String readMessageFromKafkaTopic()throws InterruptedException{
-        logger.info("running consumer");
-        this.consumer.latch.await(15, TimeUnit.SECONDS);
-        return "We read the message. Check the logs.";
     }
 }
