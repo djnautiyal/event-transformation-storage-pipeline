7f93e89a2b3413c809dd4457728c229b72fcf057
djnautiyal <dhannanjainautiyal@gmail.com>
2023-08-26 23:16:55 +0530

    Refactoring of Producer service

--- events-producer/src/main/java/com/example/eventsproducer/controller/ApiController.java
+++ events-producer/src/main/java/com/example/eventsproducer/controller/ApiController.java
@@ -1,15 +1,10 @@
 package com.example.eventsproducer.controller;
 
-import com.example.eventsproducer.service.Producer;
+import com.example.eventsproducer.constant.ApiPath;
+import com.example.eventsproducer.service.ProducerService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
-
-import java.util.concurrent.TimeUnit;
 
 //@EnableWebMvc
 @Slf4j
@@ -17,14 +12,14 @@ import java.util.concurrent.TimeUnit;
 @RequestMapping("/api")
 @RequiredArgsConstructor
 public class ApiController {
-    private Producer producer;
+    private ProducerService producerService;
 
     //TODO change it with something cooler
-    @GetMapping("/kafka/publish")
+    @GetMapping(value= ApiPath.KAFKA_PUBLISH)
     public String sendMessageToKafkaTopic(/*@RequestParam("message") String message : Later*/) {
-        log.info("running producer");
+        log.info("running producerService");
         for(int i=0; i<10; i++)
-            this.producer.sendMessage("Hello, Kafka! Random number: " + Double.valueOf(Math.random()*10000).intValue());
+            this.producerService.sendMessage("Hello, Kafka! Random number: " + Double.valueOf(Math.random()*10000).intValue());
         return "We sent the message. Hopefully, you can read them.";
     }
 }
